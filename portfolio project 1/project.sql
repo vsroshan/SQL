732ec19d9f03dce323b08c836a65ae31cdf8dc37
VS.Roshan <34996262+vsroshan@users.noreply.github.com>
2024-06-09 13:00:37 +0530

    Update project.sql

--- portfolio project 1/project.sql
+++ portfolio project 1/project.sql
@@ -1,6 +1,10 @@
+SELECT * FROM `portfolio project`.`covid deaths`
 -- SELECT location,date,total_cases,new_cases,total_deaths,population_density FROM `portfolio project`.`covid deaths`
 -- order by 1,2;
 
 SELECT location,date,total_cases,total_deaths,(total_deaths/total_cases)*100 as death_percentage FROM `portfolio project`.`covid deaths`
+where location like '%India%'
+order by 1,2;
+SELECT location,date,total_cases,population_density,(total_cases/population_density)*100 as 'affected percentage' FROM `portfolio project`.`covid deaths`
 where location like '%India%'
 order by 1,2;
